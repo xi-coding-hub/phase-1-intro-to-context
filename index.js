2f05e258469dbdd1440259277a78deff56762474
el-sam <samurai.h@yahoo.com>
2025-11-03 19:27:02 +0800

    test 4 complete

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-let employees = ['Gray', 'Worm']
+let employees = ['Gray', 'Worm', 'security']
 
 function createEmployeeRecord (arr) {
     
     let firstNm = arr[0]
     console.log(firstNm)
     let familyNm = arr[1]
-    // return (firstNm,familyNm)
+    let titleData = arr[2]
     let employeeRecord = {
        firstName: firstNm,
-       familyName: familyNm 
+       familyName: familyNm,
+       title:  titleData
     }
     return employeeRecord
 }
