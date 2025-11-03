8975185385ba78fc9771e587f6f6fd1c8eed7250
el-sam <samurai.h@yahoo.com>
2025-11-03 22:08:45 +0800

    stuck on test 9

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let employees = ['Gray', 'Worm', 'security']
+let employee = ['Gray', 'Worm', 'security']
 
 function createEmployeeRecord (arr) {
     
@@ -20,6 +20,11 @@ function createEmployeeRecord (arr) {
 }
 
 function createEmployeeRecords (){
+    let employees = [{firstName:"moe"}, {firstName:"bartholomew"} ,{firstName:""},{firstName:""},{firstName:""},{firstName:""},{firstName:""},{firstName:""},{firstName:""},{firstName:""},{firstName:""}]
+    let records = employees.map((person) => console.log(person.firstName))
+    console.log("records:",records)
     
+    return records
 }
-createEmployeeRecord(employees)
+createEmployeeRecord(employee)
+createEmployeeRecords()
